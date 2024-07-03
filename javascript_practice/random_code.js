4ced47188ada335faa28394494dffaafd62f6e86
Riya Agrawal <riyaag0105@gmail.com>
2024-07-04 01:11:33 +0530

    JavaScript beginner code + printing

---
+++ javascript_practice/random_code.js
@@ -0,0 +1,6 @@
+var carName="Volvo";
+document.getElementById("demo").innerHTML="Hello JavaScript";
+document.getElementById("demo").style.fontSize="35px";
+document.getElementById("demo").style.display="none";
+document.getElementById("demo").style.display="block";
+
