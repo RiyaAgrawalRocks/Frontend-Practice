695dc89943da137f346e0d2f071ca6af8eab7b53
Riya Agrawal <riyaag0105@gmail.com>
2024-07-04 01:50:33 +0530

    javascript basic- output+statements+ syntax+comments+variables+let

--- javascript_practice/random_code.js
+++ javascript_practice/random_code.js
@@ -4,3 +4,60 @@ document.getElementById("demo").style.fontSize="35px";
 document.getElementById("demo").style.display="none";
 document.getElementById("demo").style.display="block";
 
+//comment 1
+/* comment 2 */
+
+//   JavaScript Statements
+
+let x,y,z;
+x = 5;
+y =6 ;
+z = x + y ;
+let person= "Hege";
+
+//Variables
+
+/*
+JavaScript Variables can be declared in 4 ways:
+
+Automatically
+Using var
+Using let
+Using const
+*/
+
+x = 5;
+y = 6;
+z = x + y;
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+const x = 5;
+const y = 6;
+const z = x + y;
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+let x = "5" + 2 + 3;
+ 
+
+//Scope of Variables
+
+
+let x = 10;
+// Here x is 10
+
+{
+let x = 2;
+// Here x is 2
+}
+
+// Here x is 10
