4471bc797c8ea9f8413621666d3cce912c1ebb00
Riya Agrawal <riyaag0105@gmail.com>
2024-07-06 00:42:21 +0530

    arrays

--- javascript_practice/random_code.js
+++ javascript_practice/random_code.js
@@ -301,21 +301,168 @@ const person = {
 
   
   //template strings
-  let text = `He's often called "Johnny"`;
+ { let text = `He's often called "Johnny"`;
 
   let text = "We are the so-called \"Vikings\" from the north.";
   let text= 'It\'s alright.';
   let text = "The character \\ is called backslash.";
-
+ }
 
 //defining strings
 let x = "John";
 let y = new String("John");
 
+/*
+You can have objects in an Array. You can have functions in an Array. You can have arrays in an Array:
+*/
+myArray[0] = Date.now;
+myArray[1] = myFunction;
+myArray[2] = myCars;
+
+//array methods
+cars.length   // Returns the number of elements
+cars.sort()   // Sorts the array
+
+//looping thru array elements
+{const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+}
+
+{
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let text = "<ul>";
+fruits.forEach(myFunction);
+text += "</ul>";
+
+function myFunction(value) {
+  text += "<li>" + value + "</li>";
+}
+}
+
+//adding new array elements
+const fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Lemon");  // Adds a new element (Lemon) to fruits
 
 
+// Create an array with 40 undefined elements:
+const points = new Array(40);  
+// Create an array with one element:
+const points = [40];
 
 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits.at(2);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.join(" * ");
+//Banana * Orange * Apple * Mango
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+//removes last element
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits.pop();
+//The pop() method returns the value that was "popped out":
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi");
+//adds new element
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let length = fruits.push("Kiwi");
+//The push() method returns the new array length
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+//Orange,Apple,Mango
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits.shift();
+//The shift() method returns the value that was "shifted out"
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");
+//Lemon,Banana,Orange,Apple,Mango
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");
+//The unshift() method returns the new array length
+
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+//The concat() method creates a new array by merging (concatenating) existing arrays
+
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const myChildren = arr1.concat(arr2, arr3);
+
+const arr1 = ["Emil", "Tobias", "Linus"];
+const myChildren = arr1.concat("Peter"); 
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.copyWithin(2, 0);
+//Copy to index 2, all elements from index 0
+//Banana,Orange,Banana,Orange
+
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+fruits.copyWithin(2, 0, 2);
+//Copy to index 2, the elements from index 0 to 2
+//Banana,Orange,Banana,Orange,Kiwi,Papaya
+
+const myArr = [[1,2],[3,4],[5,6]];
+const newArr = myArr.flat();
+//1,2,3,4,5,6
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+//Banana,Orange,Lemon,Kiwi,Apple,Mango
+/*
+The first parameter (2) defines the position where new elements should be added (spliced in).
+
+The second parameter (0) defines how many elements should be removed.
+
+The rest of the parameters ("Lemon" , "Kiwi") define the new elements to be added.
+*/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
+//Banana,Orange,Lemon,Kiwi
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(0, 1);
+/*
+The first parameter (0) defines the position where new elements should be added (spliced in).
+
+The second parameter (1) defines how many elements should be removed.
+
+The rest of the parameters are omitted. No new elements will be added.
+*/
+
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const spliced = months.toSpliced(0, 1);
+//new array created
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1);
+//new array created
+//Orange,Lemon,Apple,Mango
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3);
+//Orange,Lemon
 
 
 
