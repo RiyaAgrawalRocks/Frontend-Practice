057b5e0424da2b92ddac872cee612fa358b10d0b
Riya Agrawal <riyaag0105@gmail.com>
2024-07-06 17:29:54 +0530

    Array searching

--- javascript_practice/random_code.js
+++ javascript_practice/random_code.js
@@ -465,4 +465,84 @@ const citrus = fruits.slice(1, 3);
 //Orange,Lemon
 
 
+/*
+=======================
+Array Methods
+=======================
+*/
+
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let position = fruits.indexOf("Apple") + 1;
+
+
+/*
+array.indexOf(item, start)
+item:	Required. The item to search for.
+start:	Optional. Where to start the search. Negative values will start at the given position counting from the end, and search to the end.
+Array.indexOf() returns -1 if the item is not found.
+
+If the item is present more than once, it returns the position of the first occurrence.
+*/
+
+
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let position = fruits.lastIndexOf("Apple") + 1;
+/*
+array.lastIndexOf(item, start)
+item:	Required. The item to search for
+start:	Optional. Where to start the search. Negative values will start at the given position counting from the end, and search to the beginning
+*/
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.includes("Mango"); // is true
+/*
+array.includes(search-item)
+Array.includes() allows to check for NaN values. Unlike Array.indexOf().
+*/
+
+
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+/*
+The find() method returns the value of the first array element that passes a test function.
+
+This example finds (returns the value of) the first element that is larger than 18
+*/
+
+
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.findIndex(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+/*
+The findIndex() method returns the index of the first array element that passes a test function.
+
+This example finds the index of the first element that is larger than 18
+*/
+
+
+const temp = [27, 28, 30, 40, 42, 35, 30];
+let high = temp.findLast(x => x > 40);
+//will start from the end of an array and return the value of the first element that satisfies a condition
+
+
+const temp = [27, 28, 30, 40, 42, 35, 30];
+let pos = temp.findLastIndex(x => x > 40);
+//The findLastIndex() method finds the index of the last element that satisfies a condition.
+
+
+
+/*
+=============================
+Array Sorting
+=============================
+*/
 
