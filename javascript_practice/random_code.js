e1a662b8daeb314fbfc4e1efc50fe41e7ef20ea7
Riya Agrawal <riyaag0105@gmail.com>
2024-07-07 12:14:17 +0530

    array sorting

--- javascript_practice/random_code.js
+++ javascript_practice/random_code.js
@@ -545,4 +545,133 @@ let pos = temp.findLastIndex(x => x > 40);
 Array Sorting
 =============================
 */
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+//The sort() method sorts an array alphabetically
+//creates a new array
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.reverse();
+//Mango,Apple,Orange,Banana
+//reverses the original array itself
+
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const sorted = months.toSorted();
+//sorts the original array itself
+
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const reversed = months.toReversed();
+//reverses the original array itself
+
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+//ascending order
+//1,5,10,25,40,100
+
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return b - a});
+//descending order
+//100,40,25,10,5,1
+
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(){return 0.5 - Math.random()});
+//sorting in random order
+
+
+const points = [40, 100, 1, 5, 25, 10];
+
+for (let i = points.length -1; i > 0; i--) {
+  let j = Math.floor(Math.random() * (i+1));
+  let k = points[i];
+  points[i] = points[j];
+  points[j] = k;
+}
+//Fisher Yates Shuffle
+//40,100,1,5,25,10
+
+
+/*Finding max and min values of an array:
+
+To find the lowest or highest value you have 3 options:
+
+Sort the array and read the first or last element
+Use Math.min() or Math.max()
+Write a home made function
+*/
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+// now points[0] contains the lowest value
+// and points[points.length-1] contains the highest value
+
+
+
+function myArrayMin(arr) {
+  return Math.min.apply(null, arr);
+}
+//finds lowest number in the array
+//Math.min.apply(null, [1, 2, 3]) is equivalent to Math.min(1, 2, 3).
+
+
+function myArrayMax(arr) {
+  return Math.max.apply(null, arr);
+}
+//finds max number in the array
+//Math.max.apply(null, [1, 2, 3]) is equivalent to Math.max(1, 2, 3).
+
+
+function myArrayMin(arr) {
+  let len = arr.length;
+  let min = Infinity;
+  while (len--) {
+    if (arr[len] < min) {
+      min = arr[len];
+    }
+  }
+  return min;
+}
+//fastest method to find minimum of an array
+
+
+function myArrayMax(arr) {
+  let len = arr.length;
+  let max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
+}
+//fastest method to find maximum of an array
+
+
+
+const cars = [
+  {type:"Volvo", year:2016},
+  {type:"Saab", year:2001},
+  {type:"BMW", year:2010}
+];//an object array
+
+
+//Even if objects have properties of different data types, the sort() method can be used to sort the array.
+cars.sort(function(a, b){return a.year - b.year});
+
+cars.sort(function(a, b){
+  let x = a.type.toLowerCase();
+  let y = b.type.toLowerCase();
+  if (x < y) {return -1;}
+  if (x > y) {return 1;}
+  return 0;
+});//comparing string properties
+
+
+
+
 
