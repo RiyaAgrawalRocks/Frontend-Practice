ecf96217e849d0ec8d0693ad4eec4a4107a568cb
Riya Agrawal <riyaag0105@gmail.com>
2024-07-15 02:05:26 +0530

    started promises

--- javascript_practice/promises.js
+++ javascript_practice/promises.js
@@ -3,5 +3,40 @@
 
 //Callbacks
 
+function firstFunction(parameter, callback){
+    //do stuff
+    callback();
+}
+
+//aka "callback hell"
+firstFunction(para, function(){
+    //do stuff
+    secondFunction(para, function(){
+        thirdFunction(para, function(){
+
+        });
+    });
+});
 
 
+
+/*
+=============================
+Promises
+=============================
+*/
+//Promises
+//3 states: Pending, Fulfilled, Rejected
+
+const myPromise=new Promise((resolve, reject)=>{
+const error=false;
+if(!error){
+    resolve("Yes! Resolved the Promise");
+}
+else
+{
+    reject("No! Rejected the promise");
+}
+})
+
+console.log(myPromise);
