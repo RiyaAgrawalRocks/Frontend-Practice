1b131859222878bf5f3d1d1a91a5ad4d62772f2e
Riya Agrawal <riyaag0105@gmail.com>
2025-06-03 19:35:47 +0530

    composability

--- reacttt/myapp2/firstreact/src/main.jsx
+++ reacttt/myapp2/firstreact/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MainContent(){
+  return(
+    <h1>React is great!</h1>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div>
+    <MainContent/>
+  </div>
 )
