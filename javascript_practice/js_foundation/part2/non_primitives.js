4de5cd25c811b7490c82c52f135951f9a8738f5b
Riya Agrawal <riyaag0105@gmail.com>
2025-12-15 23:44:30 +0530

    non primitives in js

---
+++ javascript_practice/js_foundation/part2/non_primitives.js
@@ -0,0 +1,27 @@
+const username = {
+  firstname: "riya",
+  isLoggedIn: true,
+  "first name": "r",
+};
+
+username.firstname = "r";
+username.lastname = "agrawal";
+
+const myFirstName = "riya";
+// myFirstName = "Riya"
+
+// console.log(myFirstName);
+
+// console.log(username["first name"]);
+
+// console.log(username.firstname);
+// console.log(username);
+// console.log(typeof username);
+
+// let today = new Date()
+// console.log(today);
+// console.log(today.getDate());
+
+//Array
+let heroes = ["a", "b", "c", true]
+let anotherUser = ['hitesh', 'choudhary']
