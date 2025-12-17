e1268ec49d4c13478d408227d5d95ad8ef723312
Riya Agrawal <riyaag0105@gmail.com>
2025-12-17 22:45:15 +0530

    js foundation complete

--- javascript_practice/js_foundation/quiz.js
+++ javascript_practice/js_foundation/quiz.js
@@ -127,6 +127,16 @@ const findLongestWord = (arr) => (
 console.log(findLongestWord(arr));
 
 
-const person = {};
+const person = {
+    name: "Hitesh",
+    age: 19.5,
+    introduce(){
+        return `Hi, my name is ${this.name} and I am ${this.age} years old`
+    }
+};
 
-function outer() {}
+function outer() {
+    function inner(){}
+    return 'Inner function called'
+}
+console.log(outer())
