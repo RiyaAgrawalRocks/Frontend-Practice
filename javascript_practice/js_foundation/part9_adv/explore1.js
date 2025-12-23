bd937e33b2248b5751817e5b3544425c9a72a4f0
Riya Agrawal <riyaag0105@gmail.com>
2025-12-23 22:52:25 +0530

    promises

--- javascript_practice/js_foundation/part9_adv/explore1.js
+++ javascript_practice/js_foundation/part9_adv/explore1.js
@@ -1,12 +1,13 @@
-console.log('chaicode');
+console.log("chaicode");
 
 function sayHello() {
-    console.log('I would like to say Hello');
+  console.log("I would like to say Hello");
 }
 
-setTim
+setTimeout(() => {
+  sayHello();
+}, 2000);
 
 for (let index = 0; index < 10; index++) {
-    console.log(index);
+  console.log(index);
 }
-
