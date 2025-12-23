2172407ff4ee4ecc751e79dbd5f80a1e093baddf
Riya Agrawal <riyaag0105@gmail.com>
2025-12-23 23:16:14 +0530

    THIS context and binding

---
+++ javascript_practice/js_foundation/part9_adv/this_context.js
@@ -0,0 +1,16 @@
+const person = {
+  name: "riya",
+  greet() {
+    console.log(`Hi, I am ${this.name}`);
+  },
+};
+
+person.greet();
+
+const greetFunction = person.greet;
+greetFunction();
+
+const boundGreet = person.greet.bind({ name: "John" });
+boundGreet();
+
+//bind, call, apply
