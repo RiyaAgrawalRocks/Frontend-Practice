5113331835ca56d650b3a78225b0cc8fb9938d5a
Riya Agrawal <riyaag0105@gmail.com>
2026-01-02 18:59:21 +0530

    todo list

--- projects/to-do-localStorage/script.js
+++ projects/to-do-localStorage/script.js
@@ -1,18 +1,61 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-let tasks = [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-addTaskButton.addEventListener("click", () => {
-  const taskText = todoInput.value.trim();
-  if (taskText === "") return;
+  tasks.forEach((task) => renderTask(task));
 
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    completed: false,
-  };
-  tasks.push(newTask);
-  todoInput.value = ''
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+
+    tasks.push(newTask);
+    saveTasks();
+    todoInput.value = "";
+    console.log(tasks);
+  });
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed)
+      li.classList.add('completed')
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>Delete</button>
+    `;
+    li.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON')
+        return
+      task.completed = !task.completed
+      li.classList.toggle('completed')
+      saveTasks()
+    })
+
+    li.querySelector('button').addEventListener('click', (e) => {
+      e.stopPropagation() //prevent toggle from firing
+      tasks = tasks.filter((t) => t.id !== task.id)
+      li.remove()
+      saveTasks()
+      renderTask(newTask)
+    })
+
+    todoList.appendChild(li)
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+
 });
+
+
