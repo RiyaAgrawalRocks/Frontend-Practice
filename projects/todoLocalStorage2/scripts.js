6411ec74d12f6b44d51cbe455ffb096a99e968ec
Riya Agrawal <riyaag0105@gmail.com>
2026-01-15 00:58:07 +0530

    js code started

---
+++ projects/todoLocalStorage2/scripts.js
@@ -0,0 +1,8 @@
+let addTaskBtn = document.getElementsByClassName('add-task-btn')[0]
+let taskList = []
+
+addTaskBtn.addEventListener('click', () => {
+    let task = document.getElementsByClassName('task-input')[0].
+    console.log(task);
+    
+})
